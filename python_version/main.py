28792adcf171e203c592d56c85c2a9ef4e4a5676
LoboAnimae <andyquan12@gmail.com>
2021-02-07 20:45:51 -0600

    Added nodes; Preparing for multithread

--- python_version/main.py
+++ python_version/main.py
@@ -6,21 +6,11 @@ class Piece:
         self.color = color
 
 
-class Game:
-    def __init__( self, turn = 1 ):
-        self.turn = turn
-
-    def take_turn( self, turn = 1 ):
-        x_coord = int( input( 'X_coord: ' ) )
-        y_coord = int( input( 'Y_coord: ' ) )
-
-
 class Table:
     def __init__( self ):
         self.table = [ [ 0 for x in range( 10 ) ] for x in range( 10 ) ]
 
     def __str__( self ):
-        print( '\n\n' )
         i = 9
         while i >= 0:
             print( '%d || ' % i, end = '' )
@@ -44,56 +34,134 @@ class Table:
 # end up in a loop, where the starting position is also the ending
 # position. For this, the starting position cannot be part of the
 # ending positions.
-def find_possible_moves( top: Table, cords: list, original_cord = None ):
-    x_cords, y_cords = cords
-    original_cords = None
-    possible_cords = [ ]
-    if original_cord is None:
-        original_cords = cords
-    try:
-        if top.table[ x_cords ][ y_cords ] == 0:
-            raise Exception( 'No piece' )
-        if cords == original_cords and original_cord is not None:
-            raise Exception( 'Original' )
 
-        around_cords = [
-            [ x_cords - 1, y_cords + 1 ], [ x_cords, y_cords + 1 ], [ x_cords + 1, y_cords + 1 ],
-            [ x_cords - 1, y_cords ], [ x_cords + 1, y_cords ],
-            [ x_cords - 1, y_cords - 1 ], [ x_cords, y_cords - 1 ], [ x_cords + 1, y_cords - 1 ]
-        ]
-        for i in around_cords:
-            x, y = i
-            if x < 0 or x > 9 or y < 0 or y > 9: continue
-            if top.table[ x ][ y ] == 0:
-                possible_cords.append( [ x, y ] )
+def game():
+    board = fill_rows()
+    print( 'Current board status: ' )
+    print( board )
 
-        for i in around_cords:
-            x, y = i
-            if x < 0 or x > 9 or y < 0 or y > 9: continue
-            if top.table[ x ][ y ] == 1:
-                for i in find_possible_moves( top, [x, y], original_cords ):
-                    x, y = i
-                    possible_cords.append( [ x, i ] )
-        return possible_cords
-    except Exception as e:
-        print (e)
-        return possible_cords
+    turn = 1
+    running = True
+    while running:
+        x = int( input( 'Piece from (X-Cord): ' ) )
+        y = int( input( 'Piece from (Y-Cord): ' ) )
+        x, y = y, x
+        result = find_possible_moves( board.table, x, y, turn )
+        print( result )
+
+
+class Node:
+    def __init__( self, x, y, player ):
+        self.x = x
+        self.y = y
+        self.objective = [ 0, 9 ] if player == 1 else [ 9, 0 ]
+        self.children = [ ]
+
+    def add_node( self, node ) -> None:
+        self.children.append( node )
+
+    def __str__( self ):
+        description = 'Node %s with the following children:\n' % str( [ self.y, self.x ] )
+        children = ''
+        for i in self.children:
+            children += '- %s \n' % str( i ) if i is not None else ''
+        if children == '':
+            return 'Node %s with no children.' % str( [ self.y, self.x ] )
+        return description + children
+
+
+def has_piece( top, x, y ):
+    if top[ x ][ y ] == 0:
+        return False
+    return True
+
+
+def is_current_player_piece( top, x, y, player ):
+    return top[ x ][ y ] == player
+
+
+def find_possible_moves( top: list, x: int, y: int, player: int, is_root = True, depth = 0 ) -> list:
+    if depth == 4:
+        return None
+    root = Node( x, y, player = player )
+    objective = [ 9, 0 ] if player == 1 else [ 9, 0 ]
+    goes_to_objective = False
+    if is_current_player_piece( top, x, y, player ):
+        goes_to_objective = True
+
+    around_cords = [
+        [ x - 1, y + 1 ], [ x, y + 1 ], [ x + 1, y + 1 ],
+        [ x - 1, y ], [ x + 1, y ],
+        [ x - 1, y - 1 ], [ x, y - 1 ], [ x + 1, y - 1 ]
+    ]
+
+    for cord in around_cords:
+        try:
+            x_cord, y_cord = cord
+            exists = has_piece( top, x_cord, y_cord )
+            if not exists and is_root:
+                root.add_node( Node( x_cord, y_cord, player ) )
+            elif exists:
+                new_x = (2 * x_cord) - x
+                new_y = (2 * y_cord) - y
+                if has_piece( top, new_x, new_y ):
+                    continue
+                root.add_node( find_possible_moves( top, new_x, new_y, player, False, depth + 1 ) )
+        except:
+            pass
+    return root
+
+    # original_cords = None
+
+
+# possible_cords = [ ]
+# if parent is None:
+#     original_cords = cords
+# try:
+#     if top.table[ x_cords ][ y_cords ] == 0:
+#         raise Exception( 'No piece' )
+#     if cords == original_cords and parent is not None:
+#         raise Exception( 'Original' )
+#
+#     around_cords = [
+#         [ x_cords - 1, y_cords + 1 ], [ x_cords, y_cords + 1 ], [ x_cords + 1, y_cords + 1 ],
+#         [ x_cords - 1, y_cords ], [ x_cords + 1, y_cords ],
+#         [ x_cords - 1, y_cords - 1 ], [ x_cords, y_cords - 1 ], [ x_cords + 1, y_cords - 1 ]
+#     ]
+#     for i in around_cords:
+#         x, y = i
+#         if x < 0 or x > 9 or y < 0 or y > 9: continue
+#         if top.table[ x ][ y ] == 0:
+#             possible_cords.append( [ x, y ] )
+#
+#     for i in around_cords:
+#         x, y = i
+#         if x < 0 or x > 9 or y < 0 or y > 9: continue
+#         if top.table[ x ][ y ] == 1:
+#             for i in find_possible_moves( top, [ x, y ], original_cords ):
+#                 x, y = i
+#                 possible_cords.append( [ x, i ] )
+#     return possible_cords
+# except Exception as e:
+#     print( e )
+#     return possible_cords
 
 
 def fill_rows():
     top = Table()
     # i = from_column - 1
-    top.table = [ [ 0, 0, 0, 0, 0, 2, 2, 2, 2, 2 ],
-                  [ 0, 0, 0, 0, 0, 0, 2, 2, 2, 2 ],
-                  [ 0, 0, 0, 0, 0, 0, 0, 2, 2, 2 ],
-                  [ 0, 0, 0, 0, 0, 0, 0, 0, 2, 2 ],
-                  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 ],
-                  [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-                  [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 ],
-                  [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 ],
-                  [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
-                  [ 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
-                  ]
+    top.table = [
+        [ 0, 0, 0, 0, 0, 2, 2, 2, 2, 2 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 2, 2, 2 ],
+        [ 0, 0, 0, 0, 0, 2, 0, 2, 2, 2 ],
+        [ 0, 0, 0, 1, 0, 0, 0, 0, 2, 2 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 ],
+        [ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 ],
+        [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 1, 1, 0, 1, 0, 0, 0, 0, 0, 0 ],
+        [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ]
+    ]
     # while i < to_column:
     #     top.table[ from_row - 1 ][ i ] = cell_type
     #     i += 1
@@ -118,11 +186,7 @@ def fill_rows():
 
 # Cords: (y, x)
 def main():
-    board = fill_rows()
-    # board.table.reverse()
-    result = find_possible_moves( board, [ 3, 9 ] )
-    print( board )
-    print( result )
+    game()
 
 
 main()
