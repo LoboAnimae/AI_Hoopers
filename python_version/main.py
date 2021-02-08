a6dee39a03252b17cb0f22eccfe12c8422a1ff4d
LoboAnimae <andyquan12@gmail.com>
2021-02-07 22:28:33 -0600

    Python main

--- python_version/main.py
+++ python_version/main.py
@@ -2,32 +2,39 @@ import numpy as np
 
 
 class Piece:
-    def __init__(self, color):
+    def __init__( self, color ):
         self.color = color
 
 
 class Game:
-    def __init__(self, turn=1):
+    def __init__( self, turn = 1 ):
         self.turn = turn
 
-    def take_turn(self, turn=1):
-        x_coord = int(input('X_coord: '))
-        y_coord = int(input('Y_coord: '))
+    def take_turn( self, turn = 1 ):
+        x_coord = int( input( 'X_coord: ' ) )
+        y_coord = int( input( 'Y_coord: ' ) )
 
 
 class Table:
-    def __init__(self):
-        self.table = [[0 for x in range(10)] for x in range(10)]
+    def __init__( self ):
+        self.table = [ [ 0 for x in range( 10 ) ] for x in range( 10 ) ]
 
-    def __str__(self):
-        print('     0 1 2 3 4 5 6 7 8 9')
-        print('     ====================')
-        for index, i in enumerate(self.table):
-            print(9 - index, end=' || ')
-            for j in i:
-                print(j, end=' ')
+    def __str__( self ):
+        print( '\n\n' )
+        i = 9
+        while i >= 0:
+            print( '%d || ' % i, end = '' )
+            for j in self.table[ i ]:
+                print( '%d ' % j, end = '' )
             print()
-
+            i -= 1
+        # for index, i in enumerate( self.table ):
+        #     print( 9 - index, end = ' || ' )
+        #     for j in i:
+        #         print( j, end = ' ' )
+        #     print()
+        print( '     ====================' )
+        print( '     0 1 2 3 4 5 6 7 8 9' )
         return ''
 
 
@@ -37,55 +44,85 @@ class Table:
 # end up in a loop, where the starting position is also the ending
 # position. For this, the starting position cannot be part of the
 # ending positions.
-def find_possible_moves(top: Table, cords: list):
+def find_possible_moves( top: Table, cords: list, original_cord = None ):
     x_cords, y_cords = cords
+    original_cords = None
+    possible_cords = [ ]
+    if original_cord is None:
+        original_cords = cords
     try:
-        if top.table[x_cords][y_cords] == 0:
-            raise Exception('No piece')
-        return True
+        if top.table[ x_cords ][ y_cords ] == 0:
+            raise Exception( 'No piece' )
+        if cords == original_cords and original_cord is not None:
+            raise Exception( 'Original' )
+
+        around_cords = [
+            [ x_cords - 1, y_cords + 1 ], [ x_cords, y_cords + 1 ], [ x_cords + 1, y_cords + 1 ],
+            [ x_cords - 1, y_cords ], [ x_cords + 1, y_cords ],
+            [ x_cords - 1, y_cords - 1 ], [ x_cords, y_cords - 1 ], [ x_cords + 1, y_cords - 1 ]
+        ]
+        for i in around_cords:
+            x, y = i
+            if x < 0 or x > 9 or y < 0 or y > 9: continue
+            if top.table[ x ][ y ] == 0:
+                possible_cords.append( [ x, y ] )
+
+        for i in around_cords:
+            x, y = i
+            if x < 0 or x > 9 or y < 0 or y > 9: continue
+            if top.table[ x ][ y ] == 1:
+                for i in find_possible_moves( top, [x, y], original_cords ):
+                    x, y = i
+                    possible_cords.append( [ x, i ] )
+        return possible_cords
     except Exception as e:
-        return e
+        print (e)
+        return possible_cords
 
 
-def fill_rows(table, from_column, to_column, from_row, cell_type):
-    top = table
-    i = from_column - 1
-    while i < to_column:
-        top.table[from_row - 1][i] = cell_type
-        i += 1
-    return top
-
-
-def create_board():
+def fill_rows():
     top = Table()
-    # Simulate C for loop
-    i = 5
-    while i >= 1:
-        top = fill_rows(top, 1, i, i + 5, 1)
-        top = fill_rows(top, i + 5, 10, i, 2)
-        i -= 1
+    # i = from_column - 1
+    top.table = [ [ 0, 0, 0, 0, 0, 2, 2, 2, 2, 2 ],
+                  [ 0, 0, 0, 0, 0, 0, 2, 2, 2, 2 ],
+                  [ 0, 0, 0, 0, 0, 0, 0, 2, 2, 2 ],
+                  [ 0, 0, 0, 0, 0, 0, 0, 0, 2, 2 ],
+                  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 ],
+                  [ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                  [ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                  [ 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 ],
+                  [ 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
+                  [ 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 ],
+                  ]
+    # while i < to_column:
+    #     top.table[ from_row - 1 ][ i ] = cell_type
+    #     i += 1
     return top
+
+
+# def create_board():
+#     top = Table()
+#     # Simulate C for loop
+#     i = 5
+#     while i >= 1:
+#         top = fill_rows( top, 1, i, i + 5, 1 )
+#         top = fill_rows( top, i + 5, 10, i, 2 )
+#         i -= 1
+#     return top
 
 
 # def main():
 #     board = create_board()
 #     print(board)
 
-board = create_board()
-# board.table[9][4] = 0
-# board.table[8][4] = 1
-# i = 0
-# j = 0
 
-board.table[0][0] = 9
-board.table[9][9] = 8
-board.table[0][9] = 7
+# Cords: (y, x)
+def main():
+    board = fill_rows()
+    # board.table.reverse()
+    result = find_possible_moves( board, [ 3, 9 ] )
+    print( board )
+    print( result )
 
-board.table.reverse()
-# for i in board.table:
-#     i.reverse()
 
-# result = find_possible_moves(board, [9, 4])
-# print(result)
-print(board)
-# main()
+main()
